1684ba49009b094b924c7446d5334a45dfcdb6e8
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2023-12-05 08:10:41 -0500

    add footer component

--- src/components/general/Footer/Footer.tsx
+++ src/components/general/Footer/Footer.tsx
@@ -1,3 +1,14 @@
+import { Divider } from "@nextui-org/react";
+
 export function Footer() {
-  return <></>;
+  return (
+    <>
+      <Divider />
+      <div className="container max-w-5xl py-2">
+        <h3 className="text-center">
+          Made with <span className="text-red-500">❤</span> by Jeff
+        </h3>
+      </div>
+    </>
+  );
 }
