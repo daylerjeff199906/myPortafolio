deb3d98cf6d598846cdd6184afbf50e84e16b5d9
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2023-12-05 16:56:12 -0500

    fixed card skill

--- src/components/general/CardSkill/CardSkill.tsx
+++ src/components/general/CardSkill/CardSkill.tsx
@@ -1,54 +1,48 @@
 import { Card, CardBody, Chip } from "@nextui-org/react";
 import { IconDialpad } from "@tabler/icons-react";
 
-interface props {
+interface Props {
   title: string;
-  otherTitle: string;
   description: string;
   tools: string[];
   other: string[];
 }
 
-export function CardSkill({ props }: { props: props }) {
+export function CardSkill({ title, description, tools, other }: Props) {
   return (
-    <>
-      <Card className="max-w-sm border-1" shadow="none">
-        <CardBody className="">
-          <div className="text-gray-500">
-            <IconDialpad size={24} />
-          </div>
-          <h3 className="text-lg font-bold">
-            {props.title || "Title default"}
-          </h3>
-          <h3 className="">
-            {props.description ||
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."}
-          </h3>
-          <div>
-            {props?.tools?.length > 0 && (
-              <h3 className="font-bold  text-sm text-gray-500">Tools</h3>
-            )}
-            {props?.tools?.length > 0 &&
-              props?.tools.map((item, index) => (
-                <Chip size="sm" key={index}>
-                  {item}
-                </Chip>
-              ))}
-          </div>
+    <Card className="max-w-sm border-1" shadow="none">
+      <CardBody>
+        <div className="text-gray-500">
+          <IconDialpad size={24} />
+        </div>
+        <h3 className="text-lg font-bold">{title || "Title default"}</h3>
+        <p>
+          {description ||
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."}
+        </p>
 
+        {tools?.length > 0 && (
           <div>
-            {props?.other?.length > 0 && (
-              <h3 className="font-bold  text-sm text-gray-500">Other</h3>
-            )}
-            {props?.other?.length > 0 &&
-              props?.other.map((item, index) => (
-                <Chip size="sm" key={index}>
-                  {item}
-                </Chip>
-              ))}
+            <h3 className="font-bold text-sm text-gray-500">Tools</h3>
+            {tools.map((item, index) => (
+              <Chip size="sm" key={index}>
+                {item}
+              </Chip>
+            ))}
           </div>
-        </CardBody>
-      </Card>
-    </>
+        )}
+
+        {other?.length > 0 && (
+          <div>
+            <h3 className="font-bold text-sm text-gray-500">Other</h3>
+            {other.map((item, index) => (
+              <Chip size="sm" key={index}>
+                {item}
+              </Chip>
+            ))}
+          </div>
+        )}
+      </CardBody>
+    </Card>
   );
 }
