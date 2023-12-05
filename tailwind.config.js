05d21a2fd50ef2609c84d8865a30a2e068bb8024
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2023-12-04 20:52:52 -0500

    fixed files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 import { nextui } from "@nextui-org/react";
-/** @type {import('tailwindcss').Config} */
 
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/pages/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}",
-    "./src/layouts/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
