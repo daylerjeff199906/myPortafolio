d6b43ef626b1fd0c90143a0f71671dec7efa92a8
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2023-12-06 13:12:18 -0500

    fixed colors

--- src/components/general/Footer/Footer.tsx
+++ src/components/general/Footer/Footer.tsx
@@ -2,11 +2,12 @@ import { Divider } from "@nextui-org/react";
 
 export function Footer() {
   return (
-    <div className="dark:bg-slate-900 text-white">
+    <div className="dark:bg-slate-900 dark:text-white">
       <Divider />
-      <div className="container max-w-5xl py-2">
+      <div className="container max-w-5xl py-4">
         <h3 className="text-center">
-          © Made with <span className="text-red-500 animate-love">❤</span> by
+          © Made with{" "}
+          <span className="text-red-500 inline-flex animate-love">❤</span> by
           Jeff | {new Date().getFullYear()}
         </h3>
       </div>
--- src/pages/client/Home/sections/landingSection.tsx
+++ src/pages/client/Home/sections/landingSection.tsx
@@ -1,11 +1,34 @@
 // import { useState, useEffect } from "react";
-import { Button } from "@nextui-org/react";
-import { IconDeviceLaptop } from "@tabler/icons-react";
+import { Button, Divider } from "@nextui-org/react";
+import {
+  IconDeviceLaptop,
+  IconBrandFacebook,
+  IconBrandPinterest,
+  IconBrandGithub,
+  IconBrandFigma,
+} from "@tabler/icons-react";
+import { Link } from "react-router-dom";
+
+const socialMedia = [
+  {
+    icon: <IconBrandGithub size={30} />,
+    url_link: "https://gist.github.com/daylerjeff199906",
+  },
+  {
+    icon: <IconBrandFacebook size={30} />,
+    url_link: "https://www.facebook.com/daylerjeff",
+  },
+  {
+    icon: <IconBrandPinterest size={30} />,
+    url_link: "https://www.pinterest.es/daylerjeff/",
+  },
+  {
+    icon: <IconBrandFigma size={30} />,
+    url_link: "https://www.figma.com/@daylerjeff",
+  },
+];
 
 export const LandingSection = () => {
-  //   const [typedText, setTypedText] = useState("");
-  //   const targetText = "I'm a Developer and Designer";
-
   const handleScrollToProjects = () => {
     const projectsSection = document.getElementById("projects");
     if (projectsSection) {
@@ -27,13 +50,26 @@ export const LandingSection = () => {
             <h3 className="text-2xl lg:text-4xl  dark:text-white">
               I'm a Developer and Designer
             </h3>
-            <Button
-              //
-              onClick={handleScrollToProjects}
-            >
-              View my works
-            </Button>
+            <Button onClick={handleScrollToProjects}>View my works</Button>
           </div>
+        </div>
+        <div className="block sm:flex">
+          <h3 className="dark:text-white">Follow me on</h3>
+          <Divider />
+          {
+            <div className="flex items-center gap-3">
+              {socialMedia.map((item, index) => (
+                <Link
+                  key={index}
+                  to={item.url_link}
+                  target="_blank"
+                  className="dark:text-white"
+                >
+                  {item.icon}
+                </Link>
+              ))}
+            </div>
+          }
         </div>
       </div>
     </>
