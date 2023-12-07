b135c2c3a3bebdf4432f7c6d38d136ea45e2e828
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2023-12-06 19:00:06 -0500

    fixed head

--- src/components/general/Splash/SplashPage.tsx
+++ src/components/general/Splash/SplashPage.tsx
@@ -1,7 +1,7 @@
 export function SplashPage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-5xl font-bold text-center">Bienvenidos</h1>
+      <h1 className="text-5xl font-bold text-center">Welcome</h1>
     </div>
   );
 }
